e37f984c069f07f24cb0f6a1874ec4d572838a84
bhanupriya03m <bhanupriyacit@gmail.com>
2024-05-24 11:56:52 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,57 @@
+
+
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/User.js";
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: "50mb"}))
+app.use(express.urlencoded({extended: true}));
+
+app.use("/api/user/", UserRoutes);
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello developers from ActiveAura",
+  });
+});
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((res) => console.log("Connected to MongoDB"))
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err.message);
+    });
+};
+
+const startServer = async () => {
+  try{
+    connectDB();
+    app.listen(8080, () =>console.log("Server running at port 8080") );
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+startServer();
